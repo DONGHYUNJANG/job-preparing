57e353fce5dc1dc7808de948d7fe8863752e120b
DONGHYUNJANG <jangdong55@gmail.com>
2026-03-23 22:42:35 +0900

    많이사용하는 exports와 alias 작성

---
+++ ExportsAlias/alias.sh
@@ -0,0 +1,7 @@
+export NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+export SQLPATH=/oracle/home/MyOraSql
+
+alias sys="sqlplus / as sysdba"
+alias adrci="adrci"
+alias dbs="cd /u01/app/oracle/product/19.3.0/dbhome_1/dbs"
+
