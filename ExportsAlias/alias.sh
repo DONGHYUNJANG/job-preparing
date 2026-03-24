321d56603c67aa83dc72713f5a12734c7f06bc60
DONGHYUNJANG <jangdong55@gmail.com>
2026-03-24 09:47:41 +0900

    alias추가

--- ExportsAlias/alias.sh
+++ ExportsAlias/alias.sh
@@ -3,5 +3,8 @@ export SQLPATH=/oracle/home/MyOraSql
 
 alias sys="sqlplus / as sysdba"
 alias adrci="adrci"
-alias dbs="cd /u01/app/oracle/product/19.3.0/dbhome_1/dbs"
+alias dbs="cd $ORACLE_HOME/dbs"
+alias oh="cd $ORACLE_HOME"
+alias ll="ls -ll"
+alias ld="ls -ld"
 
